de9f9351a49dc031122b34ad37faa34e891de54d
Mateo Meza <mezamateoj@gmail.com>
2023-03-02 00:22:30 -0300

    Add first problems solutions

---
+++ Javascript Problems/Lucky Numbers.js
@@ -0,0 +1,52 @@
+// https://exercism.org/tracks/javascript/exercises/lucky-numbers
+
+
+/**
+ * Calculates the sum of the two input arrays.
+ *
+ * @param {number[]} array1
+ * @param {number[]} array2
+ * @returns {number} sum of the two arrays
+ */
+function twoSum(array1, array2) {
+    let num1 = Number(String(array1).replace(/,/g, ''));
+    let num2 = Number(String(array2).replace(/,/g, ''));
+
+    return num1 + num2; 
+}
+
+
+
+  
+  /**
+   * Checks whether a number is a palindrome.
+   *
+   * @param {number} value
+   * @returns {boolean} whether the number is a palindrome or not
+   */
+function luckyNumber(value) {
+    let valueArray = String(value).split('');
+    let reverseValue = valueArray.reverse().join('')
+    return value === Number(reverseValue);
+  }
+
+
+  
+  /**
+   * Determines the error message that should be shown to the user
+   * for the given input value.
+   *
+   * @param {string|null|undefined} input
+   * @returns {string} error message
+   */
+function errorMessage(input) {
+    console.log(typeof(input));
+    if (typeof(input) === undefined || typeof(input) === null) {
+        return 'Required field';
+    }
+    
+    return '';
+  }
+  
+
+console.log(errorMessage('abc'));
