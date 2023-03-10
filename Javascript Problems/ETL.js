c53541ddeeefad3e82bc00b4cfea15900b4fd900
Mateo Meza <mezamateoj@gmail.com>
2023-03-09 23:41:59 -0300

    Start ETL exercise

---
+++ Javascript Problems/ETL.js
@@ -0,0 +1,17 @@
+// https://exercism.org/tracks/javascript/exercises/etl
+
+// export const transform = () => {
+//     throw new Error('Remove this statement and implement this function');
+// };
+  
+
+const oldEtl = {
+    1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
+    2: ['D', 'G'],
+    3: ['B', 'C', 'M', 'P'],
+    4: ['F', 'H', 'V', 'W', 'Y'],
+    5: ['K'],
+    8: ['J', 'X'],
+    10: ['Q', 'Z'],
+}
+
