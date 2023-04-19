d7666d2d5c65b8d67720d55de655412d89e14af9
Mateo Meza <mezamateoj@gmail.com>
2023-04-18 22:21:20 -0400

    Add task 4 and 5 solution

--- Javascript Problems/Windowing System.js
+++ Javascript Problems/Windowing System.js
@@ -35,16 +35,49 @@ class ProgramWindow {
 
     }
 
-    resize(size) {
-        this.size.resize(size);
+    // The ProgramWindow class should include a method resize. It should accept a parameter of type Size as input and attempts to resize the window to the specified size.
+    // If the window is resized to be larger than the screen, the window should be resized to the maximum size that fits on the screen.
+    // The resize method should return the new size of the window.
+    resize(newSize) {
+        this.size = newSize;
+        
+        if (this.size.width <= 0 || this.size.height <= 0) {
+            this.size = new Size(1, 1);
+        } else if (this.size.width > this.screenSize.width) {
+            this.size.width = this.screenSize.width;
+        } else if (this.size.height > this.screenSize.height) {
+            this.size.height = this.screenSize.height;
+        } 
+        return this.size;
     }
+
+    move(newPosition) {
+        this.position = newPosition;
+        if (this.position.x < 0) {
+            this.position.x = 0;
+        } else if (this.position.x > this.screenSize.width - this.size.width) {
+            this.position.x = this.screenSize.width - this.size.width;
+        }
+        if (this.position.y < 0) {
+            this.position.y = 0;
+        } else if (this.position.y > this.screenSize.height - this.size.height) {
+            this.position.y = this.screenSize.height - this.size.height;
+        }
+        return this.position;
+    }
+    
 }
 
 const programWindow = new ProgramWindow();
+const newPosition = new Position(50, 100);
 
-const newSize = new Size(600, 400);
-programWindow.resize(newSize);
-console.log(programWindow.size.width);
-// => 600
-console.log(programWindow.size.height);
+console.log(programWindow.position.x);
+console.log(programWindow.position.y);
 
+programWindow.move(newPosition);
+
+console.log(programWindow.position.x);
+console.log(programWindow.position.y);
+
+
+// task 4
