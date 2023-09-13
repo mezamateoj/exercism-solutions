f624ed0ed95af1725a36392c3ac834887f6e64d5
Mateo Meza <mezamateoj@gmail.com>
2023-09-13 00:19:20 -0300

    Add solution to gruop anagrams problem

---
+++ LeetCode/StudyRoadMap/ArraysAndHashing/groupAnagram.py
@@ -0,0 +1,21 @@
+from typing import List
+from collections import defaultdict
+
+
+# (m * n) time complexity
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+
+        for i in strs:
+            count = [0] * 26  # a...z
+
+            for char in i:
+                count[ord(char) - ord('a')] += 1
+
+            res[tuple(count)].append(i)
+        return res.values()
+
+
+test = Solution()
+print(test.groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
